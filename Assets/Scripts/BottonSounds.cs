59e7168ce3c122b63dc4f7be41175a81265779d2
Infinity6868 <Infinity6868@users.noreply.github.com>
2022-11-28 10:42:13 -0600

    ROCHA parte 4
    UI 80%
    Sonidos de fondo agregados y sonidos de boton.

---
+++ Assets/Scripts/BottonSounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonSounds : MonoBehaviour
+{
+
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot (hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
