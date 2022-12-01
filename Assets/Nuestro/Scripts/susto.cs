e353d123ba517cc6ca6157c12168a3c55d9d51ea
ikerdile <ikerdiazp13@gmail.com>
2022-12-01 05:35:02 -0600

    zombies

---
+++ Assets/Nuestro/Scripts/susto.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class susto : MonoBehaviour {
+	public Texture Image;
+	public AudioClip Sound;
+	public float timeShow = 1.5F;
+	bool show;
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.name == "ZoneScream")
+		{
+			show = true;
+		}
+	}
+	
+	void OnGUI()
+	{
+		if(show)
+		{
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Image);
+			AudioSource.PlayClipAtPoint(Sound, transform.position);
+			StartCoroutine("OFF");
+		}
+	}
+	
+	IEnumerator OFF()
+	{
+		yield return new WaitForSeconds(timeShow);
+		show = false;
+	}
+}
