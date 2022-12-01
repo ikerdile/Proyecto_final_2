3b08dc761f51a9c43abec812ce167f1993ecf735
ikerdile <ikerdiazp13@gmail.com>
2022-12-01 03:52:28 -0600

    Intentos de zombie

---
+++ Assets/Nuestro/Scripts/FXBullet_Handler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXBullet_Handler : vp_FXBullet {
+
+    protected override void DoUFPSDamage()
+    {
+        // Must be a headshot if the BoxCollider is hit
+        if(m_Hit.collider is BoxCollider)
+        {
+            Damage = 100;
+        }
+
+        // Call base method
+        base.DoUFPSDamage();
+    }
+}
